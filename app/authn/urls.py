62b23f69719b236a2ef42b35916957cecb1cf3f1
Pascal SAN <psan@bess-f4r1s5.clusters.42paris.fr>
2024-06-12 17:22:00 +0200

    fix: connect authentication system to database

--- app/authn/urls.py
+++ app/authn/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # path('devteam/', views.devteam, name='devteam'),
 
     # path('login/', views.login_view, name='login'),
     # path('register/', views.register_view, name='register'),
