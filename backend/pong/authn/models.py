c49d49e867f875d761a97f28c5780115ce135fdf
valentin-mourtialon <valentinmourtialon@protonmail.com>
2024-06-13 18:19:50 +0200

    VAL: WIP scaffolding the project

---
+++ backend/pong/authn/models.py
@@ -0,0 +1,72 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.core.mail import send_mail
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+import uuid
+
+# Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The given username must be set")
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(_("id"), primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(_("username"), max_length=150, unique=True)
+    email = models.EmailField(_("email address"), unique=True)
+    # password = models.CharField(_("password"), max_length=128) # Inherited from AbstractBaseUser
+
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+    # last_login = models.DateTimeField(_("last login"), blank=True, null=True) # Inherited from AbstractBaseUser
+
+    is_active = models.BooleanField(_("active"), default=True)
+    is_staff = models.BooleanField(_("staff status"), default=False)
+
+    objects = CustomUserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+        # abstract = True
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        send_mail(subject, message, from_email, [self.email], **kwargs)
